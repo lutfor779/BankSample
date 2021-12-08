8606ef2810beff5e354ad412c044e6185c41fdbd
lutfor779 <mdlrrahman779@gmail.com>
2021-12-08 12:33:01 +0600

    update

--- js/banking.js
+++ js/banking.js
@@ -1,23 +1,48 @@
-// deposit
-document.getElementById('deposit-btn').addEventListener('click', function () {
-    // set deposit amount
-    document.getElementById('deposit-total').innerText = parseFloat(document.getElementById('deposit-total').innerText) + parseFloat(document.getElementById('deposit-input').value);
+// update transaction
+function add(enter, main) {
+    const mainBalance = parseFloat(document.getElementById('balance-total').innerText);
+    if (mainBalance - parseFloat(enter.value) >= 0) {
+        main.innerText = parseFloat(enter.value) + parseFloat(main.innerText);
+        return main.innerText;
+    }
+}
 
-    // update balance
-    document.getElementById('balance-total').innerText = parseFloat(document.getElementById('balance-total').innerText) + parseFloat(document.getElementById('deposit-input').value);
-    
-    //set input field empty
-    document.getElementById('deposit-input').value = '';
+// update Balance
+function balance(newBalance, mainBalance, isAdd) {
+    if (isAdd == true) {
+        mainBalance.innerText = parseFloat(mainBalance.innerText) + parseFloat(newBalance.value);
+    }
+    else if (parseFloat(mainBalance.innerText) - parseFloat(newBalance.value) >= 0){
+        mainBalance.innerText = parseFloat(mainBalance.innerText) - parseFloat(newBalance.value);
+    }
+    newBalance.value = '';
+    return mainBalance.innerText;
+}
+
+// get deposit info
+document.getElementById('deposit-btn').addEventListener('click', function () {
+    const depositInput = document.getElementById('deposit-input');
+    const depositTotal = document.getElementById('deposit-total');
+    const Total = document.getElementById('balance-total');
+    if (depositInput.value > 0) {
+        add(depositInput, depositTotal);
+        balance(depositInput, Total, true);
+    }
+    else {
+        depositInput.value = '';
+    }
 })
 
-// withdraw
+// get withdraw info
 document.getElementById('withdraw-btn').addEventListener('click', function () {
-    // set withdraw amount
-    document.getElementById('withdraw-total').innerText = parseFloat(document.getElementById('withdraw-total').innerText) + parseFloat(document.getElementById('withdraw-input').value);
-
-    // update balance
-    document.getElementById('balance-total').innerText = parseFloat(document.getElementById('balance-total').innerText) - parseFloat(document.getElementById('withdraw-input').value);
-    
-    //set input field empty
-    document.getElementById('withdraw-input').value = '';
+    const withdrawInput = document.getElementById('withdraw-input');
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const Total = document.getElementById('balance-total');
+    if (withdrawInput.value > 0 ) {
+        add(withdrawInput, withdrawTotal);
+        balance(withdrawInput, Total, false);
+    }
+    else {
+        withdrawInput.value = '';
+    }
 })
